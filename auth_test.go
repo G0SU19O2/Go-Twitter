6c56353003e8204faf5f899c8f107476fb3517ea
G0SU19O2 <huyquocnguyen1902@gmail.com>
2025-08-14 22:18:50 +0700

    add test cases for RegisterInput validation and update dependencies

---
+++ auth_test.go
@@ -0,0 +1,76 @@
+package gotwitter
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestRegisterInput_Validate(t *testing.T) {
+	testCases := []struct {
+		name  string
+		input RegisterInput
+		err   error
+	}{
+		{
+			name: "valid input",
+			input: RegisterInput{
+				Username:        "validuser",
+				Email:           "test@example.com",
+				Password:        "password",
+				ConfirmPassword: "password",
+			},
+			err: nil,
+		},
+		{
+			name: "invalid email",
+			input: RegisterInput{
+				Username:        "validuser",
+				Email:           "invalid-email",
+				Password:        "password",
+				ConfirmPassword: "password",
+			},
+			err: ErrValidation,
+		},
+		{
+			name: "short password",
+			input: RegisterInput{
+				Username:        "validuser",
+				Email:           "test@example.com",
+				Password:        "short",
+				ConfirmPassword: "short",
+			},
+			err: ErrValidation,
+		},
+		{
+			name: "username too short",
+			input: RegisterInput{
+				Email:           "test@example.com",
+				Password:        "password",
+				Username:        "a",
+				ConfirmPassword: "password",
+			},
+			err: ErrValidation,
+		}, {
+			name: "passwords do not match",
+			input: RegisterInput{
+				Email:           "test@example.com",
+				Password:        "password",
+				Username:        "username",
+				ConfirmPassword: "differentpassword",
+			},
+			err: ErrValidation,
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			err := tc.input.Validate()
+			if err != nil {
+				require.ErrorIs(t, err, tc.err)
+			} else {
+				require.NoError(t, err)
+			}
+		})
+	}
+}
