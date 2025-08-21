8c26c0a9f48f67061b5e522c2525ea4d27c35b75
G0SU19O2 <huyquocnguyen1902@gmail.com>
2025-08-21 20:47:32 +0700

    Adds new PostgreSQL and utility dependencies
    Introduces additional libraries for database operations, error handling,
    and data types to support extended functionality and potential
    integration improvements.

--- go.mod
+++ go.mod
@@ -3,7 +3,9 @@ module github.com/G0SU19O2/Go-Twitter
 go 1.24.5
 
 require (
+	github.com/georgysavva/scany/v2 v2.1.4
 	github.com/golang-migrate/migrate/v4 v4.18.3
+	github.com/jackc/pgx v3.6.2+incompatible
 	github.com/jackc/pgx/v5 v5.7.5
 	github.com/joho/godotenv v1.5.1
 	github.com/stretchr/testify v1.10.0
@@ -11,13 +13,17 @@ require (
 )
 
 require (
+	github.com/cockroachdb/apd v1.1.0 // indirect
 	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/gofrs/uuid v4.4.0+incompatible // indirect
 	github.com/hashicorp/errwrap v1.1.0 // indirect
 	github.com/hashicorp/go-multierror v1.1.1 // indirect
+	github.com/jackc/fake v0.0.0-20150926172116-812a484cc733 // indirect
 	github.com/jackc/pgpassfile v1.0.0 // indirect
 	github.com/jackc/pgservicefile v0.0.0-20240606120523-5a60cdf6a761 // indirect
 	github.com/jackc/puddle/v2 v2.2.2 // indirect
 	github.com/lib/pq v1.10.9 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/rogpeppe/go-internal v1.14.1 // indirect
 	github.com/stretchr/objx v0.5.2 // indirect
---
+++ postgres/user_rego.go
@@ -0,0 +1,63 @@
+package postgres
+
+import (
+	"context"
+	"fmt"
+
+	gotwitter "github.com/G0SU19O2/Go-Twitter"
+	"github.com/georgysavva/scany/v2/pgxscan"
+	"github.com/jackc/pgx/v5"
+)
+
+type UserRepo struct {
+	DB *DB
+}
+
+func (r *UserRepo) CreateUser(ctx context.Context, user gotwitter.User) (gotwitter.User, error) {
+	tx, err := r.DB.Pool.Begin(ctx)
+	if err != nil {
+		return gotwitter.User{}, fmt.Errorf("error begin transaction: %v", err)
+	}
+	defer tx.Rollback(ctx)
+	user, err = createUser(ctx, tx, user)
+	if err != nil {
+		return gotwitter.User{}, err
+	}
+	if err := tx.Commit(ctx); err != nil {
+		return gotwitter.User{}, fmt.Errorf("error commit transaction: %v", err)
+	}
+	return user, nil
+}
+
+func createUser(ctx context.Context, tx pgx.Tx, user gotwitter.User) (gotwitter.User, error) {
+	query := `INSERT INTO users (username, email, password) VALUES ($1, $2, $3) RETURNING *`
+	u := gotwitter.User{}
+	if err := pgxscan.Get(ctx, tx, &u, query, user.Username, user.Email, user.Password); err != nil {
+		return gotwitter.User{}, fmt.Errorf("error insert: %v", err)
+	}
+	return u, nil
+}
+
+func (r *UserRepo) GetByUsername(ctx context.Context, username string) (gotwitter.User, error) {
+	query := `SELECT * FROM users WHERE username = $1 LIMIT 1`
+	u := gotwitter.User{}
+	if err := pgxscan.Get(ctx, r.DB.Pool, &u, query, username); err != nil {
+		if pgxscan.NotFound(err) {
+			return gotwitter.User{}, gotwitter.ErrNotFound
+		}
+		return gotwitter.User{}, fmt.Errorf("error select: %v", err)
+	}
+	return u, nil
+}
+
+func (r *UserRepo) GetByEmail(ctx context.Context, email string) (gotwitter.User, error) {
+	query := `SELECT * FROM users WHERE email = $1 LIMIT 1`
+	u := gotwitter.User{}
+	if err := pgxscan.Get(ctx, r.DB.Pool, &u, query, email); err != nil {
+		if pgxscan.NotFound(err) {
+			return gotwitter.User{}, gotwitter.ErrNotFound
+		}
+		return gotwitter.User{}, fmt.Errorf("error select: %v", err)
+	}
+	return u, nil
+}
